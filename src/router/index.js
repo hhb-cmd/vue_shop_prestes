7884126277b524aa575833b07c63d6ad6da5fddb
bozai <damu@example.com>
2021-08-30 19:19:30 +0800

    登陆页面功能完成了

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-undef */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+const router = new VueRouter({
   routes: [
     {
       path: '/',
@@ -13,6 +15,23 @@ export default new VueRouter({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+
+// 为路由对象 添加beforeEach前置路由守卫
+router.beforeEach((to, from, next) => {
+  // 用户访问登录页面直接放行
+  if (to.path === '/login') return next()
+  // 获取sessionStorage中保存的token
+  const token = window.sessionStorage.getItem('token')
+  // 用户访问其它页面没有token直接强制返回登录页面
+  if (!token) return next('./login')
+  next()
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="exit">退出</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    exit () {
+      // 清空sessionStorage中token
+      window.sessionStorage.clear()
+      // 退回登录页面
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
